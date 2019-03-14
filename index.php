77d7d76c91333cdf7a9883e18ebb410a1d3468ff
Arnold Villar <furthereducation77@gmail.com>
2019-03-13 23:39:33 -0400

    forms bootstrap php git putting it all together

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	<meta charset="utf-8">
+	<meta name="description" content="html5,css3,javascript">
+	<meta name="keywords" content="html5,css3,javascript">
+	<meta name="robots" content="index,follow">
+	<meta name="author" content="mySelf">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Tangerine" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=IM+Fell+English+SC" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/bootstrap.css" type= "text/css">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<title>reg exp w/net ninja</title>
+</head>
+
+<!-- End of div -->
+<body>
+<div id = "bigWrapper">
+
+<form id = "form_1" action = "#" method = "GET">
+  <fieldset id = "fieldset_1">
+    <legend id = "legend_1">New User Signup</legend>
+	<pre>
+	 <label> Name:<input id = "input_name" type="text" name = "fname"   value = ""  autocomplete = "off" size = "30" required></label><br>
+	
+        <label> Email:<input id = "input_email" type="text" name = "email"   value = ""  autocomplete = "off" size = "30" required></label><br>
+	
+     <label> password:<input id = "input_pass" type="password" name = "password" value = "" autocomplete = "off" size = "30" required></label><br>
+	
+    <label> telephone:<input id = "input_phone" type="text" name = "phone"   value = "" autocomplete = "off" size = "30"  required></label><br>
+	  
+ <label> profile slug:<input id = "input_slug" type="text" name = "slug"    value = ""  autocomplete = "off" size = "30" required></label><br>
+	
+	  <input id = "input_submit" type="submit" value="Submit">
+	  
+	</pre>  
+  </fieldset>
+</form>
+<h2 id = "demo1">testy</h2>
+</div><!-- End of bigWrapper div -->
+<script src = "main.js"></script>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,11 @@
+
+/* this is how you leave multi-line comments in javascript 
+   don't you know? */ 
+    var today = new Date();
+   var td = today.toDateString();
+   function asif(){
+	
+	 document.getElementById("demo1").innerHTML = td + "<br><span>*</span>javascript is connected";
+ };
+ 
+ asif();
